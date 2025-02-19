5be104700f0a1a93df12cf58b7597b28430b79f8
Ayan Ali <ayan_ali2004@hotmail.com>
2025-02-19 16:07:35 -0500

    added tool use logic

--- Agent.go
+++ Agent.go
@@ -1,11 +1,9 @@
 package main
 
-type Tool interface {
-}
-
 type Agent struct {
-	model   string
-	role    string
-	tools   []Tool
-	context string
+	llm    LLM    // the name of the model to use
+	role   string // the role that this agent has in the workflow
+	tools  []Tool // the list of tools that the agent has access to
+	memory string // storing information that the agent is to use as part of its recursive calls
+	cont   bool   // used to determine if the agent is to continue invoking itself or to stop
 }
--- Llm.go
+++ Llm.go
@@ -1,3 +1,7 @@
+/*
+Go implementation of Claude logic from Anthropic API
+*/
+
 package main
 
 import (
@@ -6,43 +10,32 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
-/*
-curl https://api.anthropic.com/v1/messages \
-     --header "x-api-key: $ANTHROPIC_API_KEY" \
-     --header "anthropic-version: 2023-06-01" \
-     --header "content-type: application/json" \
-     --data \
-'{
-    "model": "claude-3-5-sonnet-20241022",
-    "max_tokens": 1024,
-    "messages": [
-        {"role": "user", "content": "Hello, world"}
-    ]
-}'
-*/
-
 type LLM struct {
-	model  string
-	apiKey string
+	Model  string
+	ApiKey string
 }
 
 type LLMRequest struct {
-	model     string    `json:"model"`
-	maxTokens int       `json:"max_tokens"`
-	messages  []Message `json:"messages"`
+	Model       string    `json:"model"`
+	MaxTokens   int       `json:"max_tokens"`
+	Tools       []Tool    `json:"tools"`
+	Messages    []Message `json:"messages"`
+	System      string    `json:"system"`
+	Temperature float32   `json:"temperature"`
 }
 
 // Defines the structure of a single message (i.e. either system or user prompt)
 type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
+	Role    string `json:"role"`    // 'user' or 'assistant'
+	Content string `json:"content"` // prompt
 }
 
 type Content struct {
 	Type string `json:"type"`
-	Text string `json:"text"`
+	Text string `json:"text"` // LLM response itself
 }
 
 type ResponseMessage struct {
@@ -62,8 +55,71 @@ type LLMResponse struct {
 	Message      ResponseMessage `json:"message"`
 }
 
-func (llm LLM) call(reqData LLMRequest) (*LLMResponse, int, error) {
+/*
+Gets the LLM's response from the user's request
+*/
+func (response LLMResponse) getOutput() string {
+	var sb strings.Builder
+	for _, content := range response.Content {
+		if content.Type == "text" {
+			sb.WriteString(content.Text)
+			sb.WriteString("\n\n")
+		}
+	}
+	return sb.String()
+}
 
+/*
+Create a request for the LLM (custom messages parameter)
+*/
+func (llm LLM) NewLLMRequest(tools []Tool, maxTokens int, system string, temperature float32, messages []Message) *LLMRequest {
+	return &LLMRequest{
+		Model:       llm.Model,
+		MaxTokens:   maxTokens,
+		Tools:       tools,
+		Messages:    messages,
+		System:      system,
+		Temperature: temperature,
+	}
+}
+
+/*
+Create a request that takes in a user prompt
+*/
+func (llm LLM) NewPromptRequest(tools []Tool, model string, maxTokens int, system string, temperature float32, prompt string) *LLMRequest {
+	return &LLMRequest{
+		Model:     llm.Model,
+		MaxTokens: maxTokens,
+		Tools:     tools,
+		Messages: []Message{
+			{Role: "user", Content: prompt},
+		},
+		System:      system,
+		Temperature: temperature,
+	}
+}
+
+/*
+Create a request that involves a user prompt as well as a partially filled response
+*/
+func (llm LLM) NewPromptRequestWithResponseStarter(tools []Tool, model string, maxTokens int, system string, temperature float32, prompt string, responseStarter string) *LLMRequest {
+	return &LLMRequest{
+		Model:     llm.Model,
+		MaxTokens: maxTokens,
+		Messages: []Message{
+			{Role: "user", Content: prompt},
+			{Role: "assistant", Content: responseStarter},
+		},
+		System:      system,
+		Temperature: temperature,
+	}
+}
+
+/*
+Function for calling an LLM via a prompt.
+Returns the Response, the status code of the request, and error, if applicable
+*/
+func (llm LLM) call(reqData LLMRequest) (*LLMResponse, int, error) {
 	// Extracting the request data
 	reqBody, err := json.Marshal(reqData)
 	if err != nil {
@@ -81,7 +137,7 @@ func (llm LLM) call(reqData LLMRequest) (*LLMResponse, int, error) {
 	}
 
 	// Setting request headers
-	req.Header.Set("x-api-key:", llm.apiKey)
+	req.Header.Set("x-api-key:", llm.ApiKey)
 	req.Header.Set("anthropic-version:", "2024-10-22")
 	req.Header.Set("content-type:", "application/json")
 
@@ -90,7 +146,7 @@ func (llm LLM) call(reqData LLMRequest) (*LLMResponse, int, error) {
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error sending request %v\n", err)
-		return nil, -1, err
+		return nil, res.StatusCode, err
 	}
 
 	// Closing the response body once the function is finished executing
@@ -100,6 +156,11 @@ func (llm LLM) call(reqData LLMRequest) (*LLMResponse, int, error) {
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Printf("Error reading response %v\n", err)
+		return nil, res.StatusCode, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Printf("Request failed with status code %d: %s\n", res.StatusCode, string(resBody))
 	}
 
 	// Storing the response into the Response struct
@@ -107,9 +168,36 @@ func (llm LLM) call(reqData LLMRequest) (*LLMResponse, int, error) {
 	err = json.Unmarshal(resBody, &apiRes)
 	if err != nil {
 		fmt.Printf("Error parsing the response: %v\n", err)
-		return nil, -1, err
+		return nil, res.StatusCode, err
 	}
 
 	return &apiRes, res.StatusCode, nil
 
+}
+
+//
+/* TOOL USE LOGIC */
+//
+
+type Property struct {
+	Type        string `json:"type"`        // the data type of this property (parameter)
+	Description string `json:"description"` // a brief description of what this property (parameter) represents
+}
+
+type InputSchema struct {
+	Type       string              `json:"type"`
+	Properties map[string]Property `json:"properties"` // properties (parameters) that this tool takes as input
+	Required   []string            `json:"required"`   // names of properties that are mandatory input parameters
+}
+
+type Tool struct {
+	Name        string      `json:"name"`         // i.e. name of the function/API endpoint to call
+	Description string      `json:"description"`  // a brief description of what the tool does
+	InputSchema InputSchema `json:"input_schema"` // defining what the tool uses as input
+}
+
+type SearchTool struct {
+	apiKey      string // Brave API Key
+	name        string
+	description string
 }
